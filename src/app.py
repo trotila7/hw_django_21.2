f5ed90ed71aae9ec115612b63ebfbb4077716462
Lada Anpilogova <lada2fox@gmail.com>
2024-09-25 23:56:30 +0300

    пришлось пересоздать проект, поэтому будет тольк один коммит(

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        with open('contacts.html', 'r', encoding='utf-8') as file:
+            page_content = file.read()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
